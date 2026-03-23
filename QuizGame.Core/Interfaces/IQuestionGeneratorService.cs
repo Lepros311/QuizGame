1b3332735471a1f0aa17e0e7974e24949bb5ab16
Lepros311 <andy.strickland@gmail.com>
2026-03-23 10:24:51 -0400

    Add IQuestionGeneratorService interface

---
+++ QuizGame.Core/Interfaces/IQuestionGeneratorService.cs
@@ -0,0 +1,13 @@
+﻿using QuizGame.Core.Entities;
+using QuizGame.Core.Enums;
+
+namespace QuizGame.Core.Interfaces;
+
+public interface IQuestionGeneratorService
+{
+    Task<List<Question>> GenerateQuestionsAsync(
+        string categoryName,
+        Difficulty difficulty,
+        int questionCount,
+        List<QuestionType> questionTypes);
+}
