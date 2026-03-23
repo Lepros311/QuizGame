7033a866380fa48b0681c364514486a94c12dabe
Lepros311 <andy.strickland@gmail.com>
2026-03-23 10:20:58 -0400

    Create IQuizService interface

---
+++ QuizGame.Core/Interfaces/IQuizService.cs
@@ -0,0 +1,11 @@
+﻿using QuizGame.Core.Entities;
+using QuizGame.Core.Enums;
+
+namespace QuizGame.Core.Interfaces;
+
+public interface IQuizService
+{
+    Task<Quiz> CreateQuizAsync(string userId, int categoryId, Difficulty difficulty, int questionCount, List<QuestionType> questionTypes, bool isMultiplayer);
+    Task<Quiz?> GetQuizAsync(int quizId);
+    Task<Quiz> SubmitAnswersAsync(int quizId, Dictionary<int, string> answers);
+}
